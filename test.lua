05e6f7b80be962932fd3c8dd1645b60a585ea22c
yaya3636 <65079081+yaya3636@users.noreply.github.com>
2021-08-24 19:42:36 +0200

    Update .exe

--- test.lua
+++ test.lua
@@ -1,10 +1,11 @@
 Zone = dofile(global:getCurrentDirectory() .. "\\YAYA\\Module\\Zone.lua")
 Monsters = dofile(global:getCurrentDirectory() .. "\\YAYA\\Module\\Monsters.lua")
+Movement = dofile(global:getCurrentDirectory() .. "\\YAYA\\Module\\Movement.lua")
 Utils = dofile(global:getCurrentDirectory() .. "\\YAYA\\Module\\Utils.lua")
 
 function move()
-    Utils:Print(Zone:GetAreaName(Zone:GetArea(446)))
-    Utils:Dump(Monsters:GetMonsterSubArea(36))
-    Utils:Dump(Monsters:GetMonsterDrops(36))
-    Utils:Dump(Monsters:GetMonsterIdByDropId(2269))
+    local subAreaId = Monsters:GetFavoriteSubArea(980)
+    Utils:Print(subAreaId)
+    local tblMapId = Zone:GetSubAreaMapId(subAreaId)
+    Movement:RoadZone(tblMapId)
 end

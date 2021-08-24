65ac0b7a33492d9e7b04c7c44aee617d1614ec4e
yaya3636 <65079081+yaya3636@users.noreply.github.com>
2021-08-24 20:40:07 +0200

    Ajout Module Movement

--- test.lua
+++ test.lua
@@ -6,5 +6,9 @@ Utils = dofile(global:getCurrentDirectory() .. "\\YAYA\\Module\\Utils.lua")
 function move()
     local subAreaId = Monsters:GetFavoriteSubArea(980)
     local tblMapId = Zone:GetSubAreaMapId(subAreaId)
+
+    if Movement:InMapChecker(tblMapId) then
+        map:fight()
+    end
     Movement:RoadZone(tblMapId)
 end
